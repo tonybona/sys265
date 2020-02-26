d98c906d0c46d733bd44dfaea9580167e05d5839
tonybona <anthonybonacorsi2014Gmail.com>
2020-02-26 10:30:02 -0500

    Changes to be committed:    (use "git reset HEAD <file>..." to unstage) 	new file:   centos7/secure-ssh.sh
     Changes not staged for commit:
       (use "git add/rm <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
    
    	deleted:    ubuntu/secure-ssh.sh

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#create a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "code below"
+
